9e0ee69471a2b2b3e2bc1222255b99c440169193
takuyamikami <m.takuya0918@gmail.com>
2017-03-04 06:44:36 +0000

    共通化前まで

--- app/controllers/shirokumatters_controller.rb
+++ app/controllers/shirokumatters_controller.rb
@@ -8,8 +8,12 @@ class ShirokumattersController < ApplicationController
   end
 
   def create
-    Shirokumatter.create(shirokumatters_params)
-    redirect_to shirokumatters_path
+    @shirokumatter = Shirokumatter.new(shirokumatters_params)
+    if @shirokumatter.save
+      redirect_to shirokumatters_path, notice:"しろくまへの熱き思いをつぶやきました！"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,18 +22,25 @@ class ShirokumattersController < ApplicationController
 
   def update
     @shirokumatter = Shirokumatter.find(params[:id])
-    @shirokumatter.update(shirokumatters_params)
-    redirect_to shirokumatters_path
+    if @shirokumatter.update(shirokumatters_params)
+      redirect_to shirokumatters_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     @shirokumatter = Shirokumatter.find(params[:id])
     @shirokumatter.destroy
-    redirect_to shirokumatters_path
+    redirect_to shirokumatters_path, notice:"しろくまへの熱き思いを・・・削除して・・・しまいました・・・"
   end
 
   private
     def shirokumatters_params
       params.require(:shirokumatter).permit(:content)
+    end
+    
+    def set_shirokumatter
+      @shirokumatter = Shirokumatter.find(params[:id])
     end
 end
